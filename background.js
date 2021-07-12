0ac8fd5a55e0d39f100e5c64674d4b0081830558
tushar <tushar19340@iiitd.ac.in>
2021-07-12 16:14:04 +0530

    Animations added

--- background.js
+++ background.js
@@ -15,7 +15,7 @@ function draw() {
 
     // create a random number of snowflakes each frame
     for (let i = 0; i < 1; i++) {
-        fill("#4760ff");
+        fill("#00c8ff");
         snowflakes.push(new snowflake()); // append snowflake object
     }
 
